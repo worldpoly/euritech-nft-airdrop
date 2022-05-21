75e491ee34894573c2eb50569529e7bf28fb1b9f
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2022-05-21 15:53:32 +0300

    added chains

---
+++ src/chains.ts
@@ -0,0 +1,52 @@
+import type { AddEthereumChainParameter } from "@web3-react/types";
+
+interface BasicChainInformation {
+  urls: string[];
+  name: string;
+}
+
+interface ExtendedChainInformation extends BasicChainInformation {
+  nativeCurrency: AddEthereumChainParameter["nativeCurrency"];
+  blockExplorerUrls: AddEthereumChainParameter["blockExplorerUrls"];
+}
+
+function isExtendedChainInformation(
+  chainInformation: BasicChainInformation | ExtendedChainInformation
+): chainInformation is ExtendedChainInformation {
+  return !!(chainInformation as ExtendedChainInformation).nativeCurrency;
+}
+
+export function getAddChainParameters(chainId: number): AddEthereumChainParameter | number {
+  const chainInformation = CHAINS[chainId];
+  if (isExtendedChainInformation(chainInformation)) {
+    return {
+      chainId,
+      chainName: chainInformation.name,
+      nativeCurrency: chainInformation.nativeCurrency,
+      rpcUrls: chainInformation.urls,
+      blockExplorerUrls: chainInformation.blockExplorerUrls,
+    };
+  } else {
+    return chainId;
+  }
+}
+
+export const CHAINS: { [chainId: number]: BasicChainInformation | ExtendedChainInformation } = {
+  56: {
+    urls: ["https://bsc-dataseed.binance.org/", "https://bsc-dataseed1.defibit.io/"].filter((url) => url !== undefined),
+    name: "BSC Mainnet",
+  },
+};
+
+export const URLS: { [chainId: number]: string[] } = Object.keys(CHAINS).reduce<{ [chainId: number]: string[] }>(
+  (accumulator, chainId) => {
+    const validURLs: string[] = CHAINS[Number(chainId)].urls;
+
+    if (validURLs.length) {
+      accumulator[Number(chainId)] = validURLs;
+    }
+
+    return accumulator;
+  },
+  {}
+);
