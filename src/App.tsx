3f0d9b99968ef7759752a261ff5cc3e0e92a7b15
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2022-05-25 16:54:12 +0300

    added room route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
+import { Room } from "./pages/room";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="login" element={<Login />} />
+      <Route path="room" element={<Room />} />
       <Route path="*" element={<Login />} />
     </Routes>
   );
