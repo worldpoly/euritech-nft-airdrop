7d25bbfed9c0f2295a8578632d97d4eef8b519d1
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2022-06-01 22:12:07 +0300

    moved main logic

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { MetamaskCard } from "../components/metamask-card";
+import axios from "axios";
 import { WalletConnectCard } from "../components/wallet-connect-card";
-import { isMobile } from "react-device-detect";
+import { hooks, walletConnect } from "../connectors/walletConnect";
 import { useNavigate } from "react-router-dom";
 import { Grid } from "@mui/material";
+import { Home } from "./home";
+import { Links } from "../components/links";
 
 type LoginType = "metamask" | "walletconnect";
 
@@ -12,6 +14,12 @@ export function Login() {
   const [isLoggedIn, setIsLoggedInState] = useState<boolean>();
   const [loginType, setLoginType] = useState<LoginType>();
 
+  const { useChainId, useAccounts, useError, useIsActivating, useIsActive } = hooks;
+
+  const chainId = useChainId();
+  const accounts = useAccounts();
+  const error = useError();
+
   const setIsLoggedIn = (auth: boolean, type: LoginType) => {
     setIsLoggedInState(auth);
     setLoginType(type);
@@ -19,28 +27,52 @@ export function Login() {
 
   useEffect(() => {
     if (isLoggedIn) {
-      console.log("isLoggedIn", isLoggedIn);
-      navigateTo("/");
+      console.log("isLoggedIn", isLoggedIn, "accounts", accounts);
+      // navigateTo("/");
+      axios
+        .post(
+          "http://localhost:3002/address",
+          {
+            accounts,
+          },
+          {
+            headers: { contentType: "application/json; charset=utf-8" },
+          }
+        )
+        .then((response: any) => {
+          console.log(response);
+        })
+        .catch((error: any) => {
+          console.log(error);
+        });
     }
-  }, [isLoggedIn, navigateTo]);
+  }, [isLoggedIn, navigateTo, accounts]);
 
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      spacing={3}
-      sx={{ minHeight: "100vh!important" }}
-    >
-      {!isMobile && (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+        sx={{ minHeight: "100vh!important", height: "100%" }}
+      >
+        {
+          /*isLoggedIn &&*/ <Grid item>
+            <Home />
+          </Grid>
+        }
+        {/* !isMobile && (
         <Grid item>
           <MetamaskCard setIsLoggedIn={setIsLoggedIn} />
         </Grid>
-      )}
-      <Grid item>
-        <WalletConnectCard setIsLoggedIn={setIsLoggedIn} />
+      ) */}
+        <Grid item>
+          <WalletConnectCard setIsLoggedIn={setIsLoggedIn} />
+        </Grid>
       </Grid>
-    </Grid>
+      <Links />
+    </>
   );
 }
