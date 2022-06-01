8d539643049617c0fb3a9446d6b399c03ea02c16
Cafer Can Gündoğdu <cafercangundogdu@gmail.com>
2022-06-01 22:11:58 +0300

    check is started

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,16 @@
 import { Timer } from "../components/timer";
 
 export function Home() {
+  const timeEnd = new Date(2022, 5, 1, 12, 10, 0);
+  /*
   const time = new Date();
-  time.setSeconds(time.getSeconds() + 599); // 10min later
-  return <Timer expiryTimestamp={time} onExpire={() => {}} />;
+  time.setSeconds(time.getSeconds() + 3599); // 10min later
+  */
+  const isStarted = timeEnd.getTime() > new Date().getTime();
+  return (
+    <>
+      {isStarted && <Timer expiryTimestamp={timeEnd} onExpire={() => {}} />}
+      {/* !isStarted && <div>Not Started!</div> */}
+    </>
+  );
 }
